03a1655380eb4e44718a59faa8452209d16bd499
jesse-ctc <118642475+jesse-ctc@users.noreply.github.com>
2025-04-05 13:59:59 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import message_generate
+import os
+import discord
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    elif message.content.startswith('$help'):
+        await message.channel.send('Type the Terraria recipe name you want to know.')
+    
+    else:
+      response = message_generate.chatbot(message.content)
+      await message.channel.send(response)
+
+
+try:
+  token = os.environ["TOKEN"] or ""
+  if token == "":
+    raise Exception("Please add your token to the Secrets pane.")
+  client.run(token)
+except discord.HTTPException as e:
+    if e.status == 429:
+        print(
+            "The Discord servers denied the connection for making too many requests"
+        )
+        print(
+            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+        )
+    else:
+        raise e
